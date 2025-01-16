e2106b0430c2b46e50921fbb99b213669f0c1ca0
Muhammad Iqbal Nur Yusuf <iqbael17@gmail.com>
2025-01-16 11:39:58 +0700

    add logic feature/z

--- src/main/java/com/trunk_branch/trunk_branch/controller/ControllerA.java
+++ src/main/java/com/trunk_branch/trunk_branch/controller/ControllerA.java
@@ -2,9 +2,17 @@ package com.trunk_branch.trunk_branch.controller;
 
 public class ControllerA {
 
-    public void addFunctionA(){
+    public void addFunctionA() {
 
-    }public void addFunctionB(){
+    }
 
+    public void addFunctionB() {
+
+    }
+
+    public void addFunctionZ() {
+        System.out.println("add logic zz");
+        System.out.println("add logic zz");
+        System.out.println("add logic zz");
     }
 }
