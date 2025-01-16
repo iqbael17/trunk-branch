46006c13e25d6371c536decd621e159b84cc4e4e
Muhammad Iqbal Nur Yusuf <iqbael17@gmail.com>
2025-01-16 11:38:36 +0700

    add logic master

--- src/main/java/com/trunk_branch/trunk_branch/controller/ControllerB.java
+++ src/main/java/com/trunk_branch/trunk_branch/controller/ControllerB.java
@@ -3,6 +3,8 @@ package com.trunk_branch.trunk_branch.controller;
 public class ControllerB {
 
     public static void addFunctonZ(){
-
+        System.out.printf("add logic");
+        System.out.printf("add logic");
+        System.out.printf("add logic");
     }
 }
