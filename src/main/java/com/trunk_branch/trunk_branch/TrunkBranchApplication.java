d9681fc7227a02aefa30fe9bab38d63940e57bbd
Muhammad Iqbal Nur Yusuf <iqbael17@gmail.com>
2025-01-16 10:56:15 +0700

    first commit

---
+++ src/main/java/com/trunk_branch/trunk_branch/TrunkBranchApplication.java
@@ -0,0 +1,13 @@
+package com.trunk_branch.trunk_branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrunkBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrunkBranchApplication.class, args);
+	}
+
+}
